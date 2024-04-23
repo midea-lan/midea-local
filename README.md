c996071375bb4ac84d5d8e08c799f581c75354da
Lucas Mindêllo de Andrade <lucas@mindello.com.br>
2024-04-23 18:47:32 +0000

    feat: first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Midea-local python lib
+
+Control your Midea M-Smart appliances via local area network.
+
+This library is part of [[https://github.com/georgezhao2010/midea_ac_lan|@georgezhao2010]] code. It was separated to segregate responsabilities.
+
+⭐If this component is helpful for you, please star it, it encourages me a lot.
---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/bin/python
+"""setup midea-local."""
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+requires = ["aiohttp", "ifaddr", "pycryptodome"]
+
+setuptools.setup(
+    name="midea-local",
+    version="1.0.0",
+    author="rokam",
+    author_email="lucas@mindello.com.br",
+    description="Control your Midea M-Smart appliances via local area network",
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rokam/midea-local",
+    install_requires=requires,
+    packages=setuptools.find_packages(exclude=["tests", "tests.*"]),
+    python_requires=">=3.10",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
