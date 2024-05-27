809b059774e7d29106c68598ffc19ae478b36088
Lucas Mindêllo de Andrade <lucas@mindello.com.br>
2024-05-27 10:36:38 +0000

    test(cloud): msmart listings

--- tests/cloud_test.py
+++ tests/cloud_test.py
@@ -236,3 +236,117 @@ class CloudTest(IsolatedAsyncioTestCase):
         )
         assert cloud is not None
         assert not await cloud.login()
+
+    async def test_msmartcloud_list_home(self) -> None:
+        """Test MSmartCloud list_home"""
+        session = Mock()
+        cloud = get_midea_cloud(
+            "MSmartHome", session=session, account="account", password="password"
+        )
+        assert cloud is not None
+        homes = await cloud.list_home()
+        assert homes is not None
+        assert len(homes.keys()) == 1
+
+    async def test_msmartcloud_list_appliances(self) -> None:
+        """Test MSmartCloud list_appliances"""
+        session = Mock()
+        response = Mock()
+        response.read = AsyncMock(
+            side_effect=[
+                self.responses["msmartcloud_reroute.json"],
+                self.responses["msmartcloud_login_id.json"],
+                self.responses["msmartcloud_login.json"],
+                self.responses["msmartcloud_list_appliances.json"],
+                self.responses["invalid_response.json"],
+            ]
+        )
+        session.request = AsyncMock(return_value=response)
+        cloud = get_midea_cloud(
+            "MSmartHome", session=session, account="account", password="password"
+        )
+        assert cloud is not None
+        assert await cloud.login()
+        appliances = await cloud.list_appliances(1)
+        assert appliances is not None
+        assert len(appliances.keys()) == 2
+        assert appliances[1]["name"] == "Appliance Name"
+        assert appliances[1]["type"] == 0xAC
+        assert appliances[1]["sn"] == "1234567890abcdef1234567890abcdef"
+        assert appliances[1]["sn8"] == "0abcdef1"
+        assert appliances[1]["model_number"] == 10
+        assert appliances[1]["manufacturer_code"] == "1234"
+        assert appliances[1]["model"] == "0abcdef1"
+        assert appliances[1]["online"]
+
+        assert appliances[2]["name"] == "Appliance Name 2"
+        assert appliances[2]["type"] == 0xAC
+        assert appliances[2]["sn"] == ""
+        assert appliances[2]["sn8"] == ""
+        assert appliances[2]["model_number"] == 0
+        assert appliances[2]["manufacturer_code"] == "1234"
+        assert appliances[2]["model"] == ""
+        assert not appliances[2]["online"]
+
+        appliances = await cloud.list_appliances(1)
+        assert appliances is None
+
+    async def test_msmartcloud_get_device_info(self) -> None:
+        """Test MSmartCloud get_device_info"""
+        session = Mock()
+        response = Mock()
+        response.read = AsyncMock(
+            side_effect=[
+                self.responses["msmartcloud_reroute.json"],
+                self.responses["msmartcloud_login_id.json"],
+                self.responses["msmartcloud_login.json"],
+                self.responses["msmartcloud_list_appliances.json"],
+            ]
+        )
+        session.request = AsyncMock(return_value=response)
+        cloud = get_midea_cloud(
+            "MSmartHome", session=session, account="account", password="password"
+        )
+        assert cloud is not None
+        assert await cloud.login()
+
+        device = await cloud.get_device_info(1)
+        assert device["name"] == "Appliance Name"
+        assert device["type"] == 0xAC
+        assert device["sn"] == "1234567890abcdef1234567890abcdef"
+        assert device["sn8"] == "0abcdef1"
+        assert device["model_number"] == 10
+        assert device["manufacturer_code"] == "1234"
+        assert device["model"] == "0abcdef1"
+        assert device["online"]
+
+    async def test_msmartloud_download_lua(self) -> None:
+        """Test MSmartCloud download_lua"""
+        session = Mock()
+        response = Mock()
+        response.read = AsyncMock(
+            side_effect=[
+                self.responses["msmartcloud_reroute.json"],
+                self.responses["msmartcloud_login_id.json"],
+                self.responses["msmartcloud_login.json"],
+                self.responses["meijucloud_download_lua.json"],
+            ]
+        )
+        session.request = AsyncMock(return_value=response)
+        res = Mock()
+        res.status = 200
+        res.text = AsyncMock(return_value="4ABE0FE395F3AD3B6BC4D223F1ADFA7C")
+        session.get = AsyncMock(return_value=res)
+        cloud = get_midea_cloud(
+            "MSmartHome", session=session, account="account", password="password"
+        )
+        assert cloud is not None
+        assert await cloud.login()
+
+        if not os.path.exists("/tmp/download"):
+            os.mkdir("/tmp/download")
+        file = await cloud.download_lua("/tmp/download", 10, "00000000", "0xAC", "0010")
+        assert file is not None
+        assert os.path.exists(file)
+        os.remove(file)
+        os.removedirs("/tmp/download")
