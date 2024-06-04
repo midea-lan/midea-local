d0bad0467b388c0fb84d64681bebf502a5e8d00c
Simone Chemelli <simone.chemelli@gmail.com>
2024-06-04 23:24:01 +0900

    chore: typing db (#62)

--- midealocal/devices/db/message.py
+++ midealocal/devices/db/message.py
@@ -7,7 +7,9 @@ from ...message import (
 
 
 class MessageDBBase(MessageRequest):
-    def __init__(self, protocol_version, message_type, body_type):
+    def __init__(
+        self, protocol_version: int, message_type: int, body_type: int
+    ) -> None:
         super().__init__(
             device_type=0xDB,
             protocol_version=protocol_version,
@@ -16,12 +18,12 @@ class MessageDBBase(MessageRequest):
         )
 
     @property
-    def _body(self):
+    def _body(self) -> bytearray:
         raise NotImplementedError
 
 
 class MessageQuery(MessageDBBase):
-    def __init__(self, protocol_version):
+    def __init__(self, protocol_version: int) -> None:
         super().__init__(
             protocol_version=protocol_version,
             message_type=MessageType.query,
@@ -29,12 +31,12 @@ class MessageQuery(MessageDBBase):
         )
 
     @property
-    def _body(self):
+    def _body(self) -> bytearray:
         return bytearray([])
 
 
 class MessagePower(MessageDBBase):
-    def __init__(self, protocol_version):
+    def __init__(self, protocol_version: int) -> None:
         super().__init__(
             protocol_version=protocol_version,
             message_type=MessageType.set,
@@ -43,7 +45,7 @@ class MessagePower(MessageDBBase):
         self.power = False
 
     @property
-    def _body(self):
+    def _body(self) -> bytearray:
         power = 0x01 if self.power else 0x00
         return bytearray(
             [
@@ -73,7 +75,7 @@ class MessagePower(MessageDBBase):
 
 
 class MessageStart(MessageDBBase):
-    def __init__(self, protocol_version):
+    def __init__(self, protocol_version: int) -> None:
         super().__init__(
             protocol_version=protocol_version,
             message_type=MessageType.set,
@@ -83,7 +85,7 @@ class MessageStart(MessageDBBase):
         self.washing_data = bytearray([])
 
     @property
-    def _body(self):
+    def _body(self) -> bytearray:
         if self.start:  # Pause
             return bytearray([0xFF, 0x01]) + self.washing_data
         else:
@@ -92,25 +94,24 @@ class MessageStart(MessageDBBase):
 
 
 class DBGeneralMessageBody(MessageBody):
-    def __init__(self, body):
+    def __init__(self, body: bytearray) -> None:
         super().__init__(body)
         self.power = body[1] > 0
         self.start = True if body[2] in [2, 6] else False
         self.washing_data = body[3:16]
         self.progress = 0
+        self.time_remaining: float | None = None
         for i in range(7):
             if (body[16] & (1 << i)) > 0:
                 self.progress = i + 1
                 break
         if self.power:
             self.time_remaining = body[17] + (body[18] << 8)
-        else:
-            self.time_remaining = None
 
 
 class MessageDBResponse(MessageResponse):
-    def __init__(self, message):
-        super().__init__(message)
+    def __init__(self, message: bytes) -> None:
+        super().__init__(bytearray(message))
         if self.message_type in [MessageType.query, MessageType.set] or (
             self.message_type == MessageType.notify1 and self.body_type == 0x04
         ):
