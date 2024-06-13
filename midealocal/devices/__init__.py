752e0ef4b94b89b37ccc6e4b8a461d2c3ccb3b86
Lucas Mindêllo de Andrade <lucas@mindello.com.br>
2024-06-13 17:58:12 +0000

    chore(devices): revert code

--- midealocal/devices/__init__.py
+++ midealocal/devices/__init__.py
@@ -58,9 +58,9 @@ def device_selector(
 ) -> Any:
     try:
         if device_type < DeviceType.A0:
-            device_path = f".{'x{:02x}'.format(device_type)}"
+            device_path = f".{'x%02x' % device_type}"
         else:
-            device_path = f".{'{:02x}'.format(device_type)}"
+            device_path = f".{'%02x' % device_type}"
         module = import_module(device_path, __package__)
         device = module.MideaAppliance(
             name=name,
