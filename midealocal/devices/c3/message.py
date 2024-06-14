f2332620a2acd22f7405140816a4cb5676c0150f
Lucas Mindêllo de Andrade <lucas@mindello.com.br>
2024-06-14 19:26:35 +0000

    docs(c3): add docstring

--- midealocal/devices/c3/message.py
+++ midealocal/devices/c3/message.py
@@ -1,3 +1,5 @@
+"""Midea local C3 message."""
+
 from midealocal.devices import BodyType
 from midealocal.message import (
     MessageBody,
@@ -10,12 +12,15 @@ TEMP_NEG_VALUE = 127
 
 
 class MessageC3Base(MessageRequest):
+    """C3 message base."""
+
     def __init__(
         self,
         protocol_version: int,
         message_type: int,
         body_type: int,
     ) -> None:
+        """Initialize C3 message base."""
         super().__init__(
             device_type=0xC3,
             protocol_version=protocol_version,
@@ -29,7 +34,10 @@ class MessageC3Base(MessageRequest):
 
 
 class MessageQuery(MessageC3Base):
+    """C3 message query."""
+
     def __init__(self, protocol_version: int) -> None:
+        """Initialize C3 message query."""
         super().__init__(
             protocol_version=protocol_version,
             message_type=MessageType.query,
@@ -42,7 +50,10 @@ class MessageQuery(MessageC3Base):
 
 
 class MessageSet(MessageC3Base):
+    """C3 message set."""
+
     def __init__(self, protocol_version: int) -> None:
+        """Initialize C3 message set."""
         super().__init__(
             protocol_version=protocol_version,
             message_type=MessageType.set,
@@ -90,7 +101,10 @@ class MessageSet(MessageC3Base):
 
 
 class MessageSetSilent(MessageC3Base):
+    """C3 message set silent."""
+
     def __init__(self, protocol_version: int) -> None:
+        """Initialize C3 message set silent."""
         super().__init__(
             protocol_version=protocol_version,
             message_type=MessageType.set,
@@ -120,7 +134,10 @@ class MessageSetSilent(MessageC3Base):
 
 
 class MessageSetECO(MessageC3Base):
+    """C3 message set eco."""
+
     def __init__(self, protocol_version: int) -> None:
+        """Initialize C3 message set eco."""
         super().__init__(
             protocol_version=protocol_version,
             message_type=MessageType.set,
@@ -136,6 +153,8 @@ class MessageSetECO(MessageC3Base):
 
 
 class C3MessageBody(MessageBody):
+    """C3 message body."""
+
     def __init__(self, body: bytearray, data_offset: int = 0) -> None:
         super().__init__(body)
         self.zone1_power = body[data_offset + 0] & 0x01 > 0
@@ -170,7 +189,10 @@ class C3MessageBody(MessageBody):
 
 
 class C3Notify1MessageBody(MessageBody):
+    """C3 notify1 message body."""
+
     def __init__(self, body: bytearray, data_offset: int = 0) -> None:
+        """Initialize C3 notify1 message body."""
         super().__init__(body)
         status_byte = body[data_offset]
         self.status_tbh = (status_byte & 0x08) > 0
@@ -198,7 +220,10 @@ class C3Notify1MessageBody(MessageBody):
 
 
 class MessageC3Response(MessageResponse):
+    """C3 message response."""
+
     def __init__(self, message: bytes) -> None:
+        """Initialize C3 message response."""
         super().__init__(bytearray(message))
         if (
             self.message_type
