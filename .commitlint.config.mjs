9d81b19b8dfdb2267292e9cfc82144f754ce5e01
Simone Chemelli <simone.chemelli@gmail.com>
2024-08-28 09:10:21 +0000

    chore(config): fix syntax

--- .commitlint.config.mjs
+++ .commitlint.config.mjs
@@ -1,5 +1,15 @@
 const Configuration = {
+  /*
+   * Resolve and load @commitlint/config-conventional from node_modules.
+   * Referenced packages must be installed
+   */
+  extends: ["@commitlint/config-conventional"],
+  /*
+   * Any rules defined here will override rules from @commitlint/config-conventional
+   */
   rules: {
     "body-max-line-length": "Infinity",
   },
 };
+
+export default Configuration;
